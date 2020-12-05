d716fa4a381c8f0a0be403828301645ec1afebcc
Geoff Rich <geoff.rich@alaskaair.com>
2020-12-05 09:12:08 -0800

    Finish solution 1

--- solutions/1.js
+++ solutions/1.js
@@ -1,16 +1,30 @@
 import inputArr from '../inputs/1';
 
 export default function solvePuzzle() {
+  return [getTwoEntries(2020), getThreeEntries()];
+}
+
+function getTwoEntries(goal) {
   const numbersToLookFor = new Set();
 
   for (let i = 0; i < inputArr.length; ++i) {
     const num = inputArr[i];
-    const distanceFrom2020 = 2020 - num;
+    const distanceFromGoal = goal - num;
     if (numbersToLookFor.has(num)) {
-      return num * distanceFrom2020;
+      console.log(numbersToLookFor);
+      return num * distanceFromGoal;
     }
-    numbersToLookFor.add(distanceFrom2020);
+    numbersToLookFor.add(distanceFromGoal);
   }
+  return -1;
+}
 
-  console.log(numbersToLookFor);
+function getThreeEntries() {
+  for (let num of inputArr) {
+    const solution = getTwoEntries(2020 - num);
+    if (solution !== -1) {
+      return solution * num;
+    }
+  }
+  return -1;
 }

658c7795e07487814921984b8dcef54f965a570f
Geoff Rich <geoff.rich@alaskaair.com>
2020-12-18 20:33:30 -0800

    Solution 15b

--- solutions/15.js
+++ solutions/15.js
@@ -1,7 +1,7 @@
-import { testInput as inputArr, finalInput } from '../inputs/15';
+import { testInput, finalInput as inputArr } from '../inputs/15';
 
 export default function solvePuzzle() {
-  return [getNumberSpoken(2020)];
+  return [getNumberSpoken(2020), getNumberSpoken(30000000)];
 }
 
 function getNumberSpoken(count) {
@@ -10,29 +10,15 @@ function getNumberSpoken(count) {
   for (let i = 0; i < count; i++) {
     if (i < inputArr.length) {
       spokenNumber = inputArr[i];
-      numLookup.set(spokenNumber, [i]);
+      numLookup.set(spokenNumber, i);
     } else {
       const val = numLookup.get(spokenNumber);
-      if (val.length === 1) {
-        spokenNumber = 0;
+      numLookup.set(spokenNumber, i - 1);
+      if (val !== undefined) {
+        spokenNumber = i - 1 - val;
       } else {
-        const [x, y] = val;
-        spokenNumber = y - x;
+        spokenNumber = 0;
       }
-      updateMap(spokenNumber, i);
-      console.log(i, spokenNumber);
-    }
-  }
-
-  function updateMap(num, turn) {
-    const val = numLookup.get(num);
-    if (val === undefined) {
-      numLookup.set(num, [turn]);
-    } else if (val.length === 1) {
-      numLookup.set(num, [...val, turn]);
-    } else {
-      const [x, y] = val;
-      numLookup.set(num, [y, turn]);
     }
   }
 
